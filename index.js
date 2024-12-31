4d3b1498260236ef35b8e3d2b8909c0075eb7cc9
Ahmed Yehia <98207790+AhmedHosny2@users.noreply.github.com>
2024-12-31 14:42:09 +0100

    clean script

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 // Endpoint to serve the PowerShell script
-app.get('/api/download', async (req, res) => {
+app.get('/download', async (req, res) => {
     try {
         const scriptPath = path.join(__dirname, '.', 'scripts', 'agent_vm_ulm.ps1');
         const scriptContent = await fs.readFile(scriptPath, 'utf-8');
@@ -20,20 +20,6 @@ app.get('/api/download', async (req, res) => {
     }
 });
 
-// Endpoint to serve the installer script
-app.get('/api/installer', async (req, res) => {
-    try {
-        const installerPath = path.join(__dirname, '..', 'public', 'install_and_run.ps1');
-        const installerContent = await fs.readFile(installerPath, 'utf-8');
-
-        res.setHeader('Content-Disposition', 'attachment; filename=install_and_run.ps1');
-        res.setHeader('Content-Type', 'text/plain');
-        res.status(200).send(installerContent);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Failed to load installer script' });
-    }
-});
 
 // Default route
 app.get('/', (req, res) => {
